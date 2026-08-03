6add08d8886fad3d8a4fda085b3c92d78255365c
Kerim <sunabmey@gmail.com>
2026-08-03 06:35:55 +0300

    Create task input user interface

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("Task Manager Loaded");
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+
+    const task = taskInput.value.trim();
+
+    if(task===""){
+
+        alert("Please enter a task");
+
+        return;
+
+    }
+
+    const li=document.createElement("li");
+
+    li.textContent=task;
+
+    taskList.appendChild(li);
+
+    taskInput.value="";
+
+});
